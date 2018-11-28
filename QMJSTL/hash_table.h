11cfe5c2b67a1720eef95af4e5c06a5d121d4d00
Galibier <cx.sjtu@gmail.com>
2018-11-28 20:04:24 +0800

    STL

--- QMJSTL/hash_table.h
+++ QMJSTL/hash_table.h
@@ -233,6 +233,137 @@ namespace qmj {
         auto pos = qmj::lower_bound(first, last, n);
         return pos == last ? *(last - 1) : *pos;
     }
+
+    template<typename traits>
+    class hashtable {
+    public:
+        friend class hashtable_const_iterator<traits>;
+
+        friend class hashtable_iterator<traits>;
+
+        typedef typename traits::allocator_type allocator_type;
+        //typedef typename traits::HashFunction hashfunction;
+        typedef typename traits::HashFunction hasher;
+        typedef typename traits::EqualKey equalkey;
+        typedef typename traits::EqualKey key_equal;
+        typedef typename traits::key_type key_type;
+        typedef typename traits::value_type value_type;
+        typedef value_type *pointer;
+        typedef const value_type *const_pointer;
+        typedef value_type &reference;
+        typedef const value_type &const_reference;
+        typedef size_t size_type;
+        typedef std::ptrdiff_t difference_type;
+
+        typedef typename allocator_type::template rebind<hashtable_node<value_type>>::other alloc;
+        typedef hashtable_const_iterator<traits> const_iterator;
+        typedef hashtable_const_local_iterator<traits> const_local_iterator;
+        typedef typename If<is_same<key_type, value_type>::value, const_iterator,
+                hashtable_iterator<traits>>::type iterator;
+        typedef typename If<is_same<key_type, value_type>::value, const_local_iterator,
+                hashtable_local_iterator<traits>>::type local_iterator;
+        enum {
+            is_multi = traits::is_multi
+        };
+
+        typedef hashtable_node<value_type> *link_type;
+        typedef hashtable<traits> self;
+        typedef qmj::vector<link_type> container;
+
+        typedef std::pair<iterator, bool> PairIB;
+        typedef std::pair<local_iterator, local_iterator> PairII;
+        typedef std::pair<const_local_iterator, const_local_iterator> PairCC;
+        typedef std::pair<link_type, link_type> PairLL;
+
+        hashtable() : hash(), equals(), num_elements(0) { init_buckets(0); }
+
+        explicit hashtable(size_t n) : hash(), equals(), num_elements(0) {
+            init_buckets(n);
+        }
+
+        hashtable(size_t n, const hasher &hash)
+                : hash(hash), equals(), num_elements(0) {
+            init_buckets(n);
+        }
+
+        hashtable(const hasher &hash, const equalkey &equals)
+                : hash(hash), equals(equals), num_elements(0) {
+            init_buckets(0);
+        }
+
+        hashtable(const size_t n, const hasher &hash, const equalkey &equals)
+                : hash(hash), equals(equals), num_elements(0) {
+            init_buckets(n);
+        }
+
+        hashtable(const self &x)
+                : buckets(x.buckets.size(), nullptr), hash(x.hash),
+                  equals(x.equals), num_elements(x.num_elements) {
+            for (size_t i = 0; i != buckets.size(); ++i)
+                for (link_type cur = x.buckets[i]; cur; cur = cur->next)
+                    buckets[i] = create_node(cur->value, buckets[i]);
+        }
+
+        hashtable(self &&x)
+                : num_elements(x.num_elements), hash(std::move(x.hash)),
+                  equals(std::move(x.equals)), buckets(std::move(x.buckets)) {}
+
+        self &operator=(self x) {
+            hash = x.hash;
+            equals = x.equals;
+            num_elements = x.num_elements;
+            buckets.swap(x.buckets);
+            return (*this);
+        }
+
+        ~hashtable() { clear(); }
+
+        void swap(self &x) noexcept {
+            swap(hash, x.hash);
+            swap(equals, x.equals);
+            std::swap(num_elements, x.num_elements);
+            buckets.swap(x.buckets);
+        }
+
+        iterator begin() {
+            if (empty())
+                return (end());
+            size_t i = 0;
+            for (const size_t len = buckets.size(); i != len && (!buckets[i]);)
+                ++i;
+            return (iterator(buckets[i], this));
+        }
+
+
+    private:
+        void init_buckets(const size_type n) {
+            const size_type n_buckets = next_prime(n);
+            buckets.resize(n_buckets, nullptr);
+        }
+
+        void clear() {
+            const size_t len = buckets.size();
+            for (size_t i = 0; i != len; ++i)
+                for (link_type cur = buckets[i], next; cur; cur = next) {
+                    next = cur->next;
+                    destroy_and_free_node(cur);
+                }
+
+            memset(&buckets[0], 0, len);
+            num_elements = 0;
+        }
+
+        void destroy_and_free_node(link_type node) {
+            alloc::destroy(node);
+            alloc::deallocate(node);
+        }
+
+    private:
+        hasher hash;
+        key_equal equals;
+        container buckets;
+        size_type num_elements;
+    };
 }
 
 #endif //_HASH_TABLE_
